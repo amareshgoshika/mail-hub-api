09b00dbd64c2f3dde46a82f302775656ef7f4b64
Amaresh Goshika <amargoshika@gmail.com>
2024-12-30 15:16:50 -0600

    updated env

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,8 +1,21 @@
-// firebase-admin.js
 const admin = require('firebase-admin');
+require('dotenv').config();
 
-// Initialize Firebase Admin SDK with your Firebase service account key
-const serviceAccount = require('./firebase-account.json'); // Replace with the path to your Firebase service account key file
+const privateKey = process.env.REACT_APP_FIREBASE_PRIVATE_KEY;
+
+if (!privateKey) {
+  throw new Error("Firebase private key is not defined.");
+}
+
+// Initialize Firebase Admin SDK with environment variables
+const serviceAccount = {
+  type: 'service_account',
+  project_id: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  private_key_id: process.env.REACT_APP_FIREBASE_PRIVATE_KEY_ID,
+  private_key: privateKey.replace(/\\n/g, '\n'),
+  client_email: process.env.REACT_APP_FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.REACT_APP_FIREBASE_CLIENT_ID,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
