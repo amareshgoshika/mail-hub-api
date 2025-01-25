bc5c863f61db530479630504c078a99d1609de59
Amaresh Goshika <amargoshika@gmail.com>
2025-01-25 14:59:56 -0600

    Created contact form

--- home-api.js
+++ home-api.js
@@ -1,7 +1,34 @@
 const express = require('express');
-const admin = require('firebase-admin');
 const { db } = require('./firebase-admin');
 const router = express.Router();
+const path = require('path');
+const { google } = require('googleapis');
+const fs = require('fs');
+
+const redirect_uri = process.env.REACT_APP_REDIRECT_URL;
+const persistentDiskPath = '/var/data/resumes';
+
+async function getService(adminMail) {
+  const credentialsFile = path.join(persistentDiskPath, adminMail, 'credentials.json');
+  const tokenFile = path.join(persistentDiskPath, adminMail, 'token.pickle');
+
+  if (!fs.existsSync(credentialsFile)) {
+    throw new Error("Credentials file 'credentials.json' not found. Upload it first.");
+  }
+
+  const credentials = JSON.parse(fs.readFileSync(credentialsFile));
+  const { client_secret, client_id} = credentials.web;
+  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uri);
+
+  if (fs.existsSync(tokenFile)) {
+    const token = fs.readFileSync(tokenFile);
+    oAuth2Client.setCredentials(JSON.parse(token));
+  } else {
+    throw new Error('Token not found. Authenticate first.');
+  }
+
+  return google.gmail({ version: 'v1', auth: oAuth2Client });
+}
 
 router.get('/get-pricing-plans', async (req, res) => {
     try {
@@ -27,4 +54,52 @@ router.get('/get-pricing-plans', async (req, res) => {
     }
   });
   
+  router.post('/send-contact-us-email', async (req, res) => {
+    try {
+      const {subject: name, userEmail, emailBody } = req.body;
+
+      const adminMail = "contact.maileazy@gmail.com"
+  
+      if (!name || !emailBody) {
+        return res.status(400).json({ error: 'Recipient email, subject, and email body are required' });
+      }
+  
+      const service = await getService(adminMail);
+      const rawMessage = createEmail(adminMail, "CF: " + name + " " + userEmail, emailBody);
+      
+      const response = await service.users.messages.send({
+        userId: 'me',
+        requestBody: {
+          raw: rawMessage,
+        },
+      });
+  
+      res.json({ message: 'Email sent successfully', response });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
+  function createEmail(adminMail, name, emailBody) {
+    const boundary = "__boundary__";
+  
+    const messageParts = [
+      `To: ${adminMail}`,
+      `Subject: ${name}`,
+      `MIME-Version: 1.0`,
+      `Content-Type: multipart/mixed; boundary="${boundary}"`,
+      '',
+      `--${boundary}`,
+      `Content-Type: text/html; charset="UTF-8"`,
+      'Content-Transfer-Encoding: 7bit',
+      '',
+      emailBody,
+      '',
+    ];
+  
+    messageParts.push(`--${boundary}--`);
+    return Buffer.from(messageParts.join('\r\n')).toString('base64');
+  }
+
+
 module.exports = router;
